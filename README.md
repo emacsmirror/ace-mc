ced28c9bb8b09c1f57d069f9b19fb756cc1bb94f
Josh Moller-Mara <j.mollermara@nyu.edu>
2016-02-29 00:08:01 -0500

    More consistent naming

--- README.md
+++ README.md
@@ -12,24 +12,24 @@ ace jump mode.
 # Usage
 
 Ace-mc comes with two commands:
-* `ajmc/add-multiple-cursors`
-* `ajmc/add-single-cursor`.
+* `ace-mc/add-multiple-cursors`
+* `ace-mc/add-single-cursor`.
 
 Both do pretty much the same thing.
 
 I have `ace-jump-mode` bound to <kbd>C-0</kbd>, so I bind
-`ajmc/add-multiple-cursors` to <kbd>C-)</kbd> and
-`ajmc/add-single-cursor` to <kbd>M-C-)</kbd>. The setup would be like
+`ace-mc/add-multiple-cursors` to <kbd>C-)</kbd> and
+`ace-mc/add-single-cursor` to <kbd>M-C-)</kbd>. The setup would be like
 this:
 
 ```elisp
-(global-set-key (kbd "C-)") 'ajmc/add-multiple-cursors)
-(global-set-key (kbd "M-C-)") 'ajmc/add-single-cursor)
+(global-set-key (kbd "C-)") 'ace-mc/add-multiple-cursors)
+(global-set-key (kbd "M-C-)") 'ace-mc/add-single-cursor)
 ```
 
-`ajmc/add-multiple-cursors` prompts for a "Query Char" for the first
+`ace-mc/add-multiple-cursors` prompts for a "Query Char" for the first
 character of a word, much in the same way that Ace Jump does. In fact,
-`ajmc/add-multiple-cursors` takes similar prefix arguments that
+`ace-mc/add-multiple-cursors` takes similar prefix arguments that
 `ace-jump-mode` does. So if you pass one <kbd>C-u</kbd> prefix to it, it'll
 activate `ace-jump-char-mode`, and with <kbd>C-u</kbd> <kbd>C-u</kbd> it'll activate
 `ace-jump-line-mode`.
@@ -44,5 +44,5 @@ region. When you have an active region, no "query char" is prompted
 for. Instead, you just get a list of locations that match the text in
 your region.
 
-`ajmc/add-single-cursor` does the same thing as
-`ajmc/add-multiple-cursors`, just without looping.
+`ace-mc/add-single-cursor` does the same thing as
+`ace-mc/add-multiple-cursors`, just without looping.
--- ace-mc.el
+++ ace-mc.el
@@ -6,7 +6,7 @@
 ;; Version: 1.0
 ;; Package-Requires ((ace-jump-mode "1.0") (multiple-cursors "1.0"))
 ;; Keywords: motion, location, cursor
-;; URL: https://github.com/mm--/ace-jump-multiple-cursors
+;; URL: https://github.com/mm--/ace-mc
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -26,85 +26,85 @@
 ;; ace-mc.el is a package that allows you to add a Multiple Cursors
 ;; mode cursor using ace-jump.
 
-;; This package adds the command `ajmc/add-multiple-cursors', which
+;; This package adds the command `ace-mc/add-multiple-cursors', which
 ;; acts like `ace-jump-mode'. It will continue to keep prompting for
 ;; places to add cursors until you hit Enter.
 ;;
-;; You can use the command `ajmc/add-single-cursor' for a non-looping
-;; version of `ajmc/add-multiple-cursors'.
+;; You can use the command `ace-mc/add-single-cursor' for a non-looping
+;; version of `ace-mc/add-multiple-cursors'.
 
 ;; If you have ace-jump bound on C-0, for example, I recommend the
 ;; following key bindings:
 ;;
-;; (global-set-key (kbd "C-)") 'ajmc/add-multiple-cursors)
-;; (global-set-key (kbd "C-M-)") 'ajmc/add-single-cursor)
+;; (global-set-key (kbd "C-)") 'ace-mc/add-multiple-cursors)
+;; (global-set-key (kbd "C-M-)") 'ace-mc/add-single-cursor)
 
 ;;; Code:
 (require 'ace-jump-mode)
 (require 'multiple-cursors-core)
 (require 'dash)
 
-(defvar ajmc/marking nil
+(defvar ace-mc/marking nil
   "Internal flag for detecting if currently marking.")
 
-(defvar ajmc/keyboard-reset nil
+(defvar ace-mc/keyboard-reset nil
   "See if we've quit out yet.")
 
-(defvar ajmc/query-char nil
+(defvar ace-mc/query-char nil
   "Char.")
 
-(defvar ajmc/loop-marking nil
+(defvar ace-mc/loop-marking nil
   "Keep adding until we quit.")
 
-(defvar ajmc/saved-point nil
+(defvar ace-mc/saved-point nil
   "The position of our cursor before jumping around with ace-jump.")
 
 ;; TODO: Fix description
-(defvar ajmc/ace-mode-function nil
+(defvar ace-mc/ace-mode-function nil
   "The function from `ace-jump-mode-submode-list` to use.")
 
-(defun ajmc/maybe-jump-start ()
+(defun ace-mc/maybe-jump-start ()
   "Push the mark when marking with `ace-jump-char-mode'."
-  (when ajmc/marking
-    (setq ajmc/saved-point (point)
-	  ajmc/keyboard-reset nil)))
+  (when ace-mc/marking
+    (setq ace-mc/saved-point (point)
+	  ace-mc/keyboard-reset nil)))
 
-(defun ajmc/maybe-jump-end ()
+(defun ace-mc/maybe-jump-end ()
   "Zap after marking with `ace-jump-char-mode.'."
-  (if (not ajmc/marking)
-      (ajmc/reset)
-    (let ((ajmc/fake-cursor-at-point (-filter 'mc/fake-cursor-p (overlays-at (point)))))
-      (if ajmc/fake-cursor-at-point
-	  (mc/remove-fake-cursor (car ajmc/fake-cursor-at-point))
-	(unless (equal ajmc/saved-point (point))
+  (if (not ace-mc/marking)
+      (ace-mc/reset)
+    (let ((ace-mc/fake-cursor-at-point (-filter 'mc/fake-cursor-p (overlays-at (point)))))
+      (if ace-mc/fake-cursor-at-point
+	  (mc/remove-fake-cursor (car ace-mc/fake-cursor-at-point))
+	(unless (equal ace-mc/saved-point (point))
 	  (mc/create-fake-cursor-at-point))))
     (mc/maybe-multiple-cursors-mode)
-    (when ajmc/saved-point
-      (goto-char ajmc/saved-point))
-    (if (and ajmc/loop-marking (not ajmc/keyboard-reset))
-	(ajmc/add-char ajmc/query-char)
-      (ajmc/reset))))
+    (when ace-mc/saved-point
+      (goto-char ace-mc/saved-point))
+    (if (and ace-mc/loop-marking (not ace-mc/keyboard-reset))
+	(ace-mc/add-char ace-mc/query-char)
+      (ace-mc/reset))))
 
-(add-hook 'ace-jump-mode-before-jump-hook #'ajmc/maybe-jump-start)
+(add-hook 'ace-jump-mode-before-jump-hook #'ace-mc/maybe-jump-start)
 
-(add-hook 'ace-jump-mode-end-hook #'ajmc/maybe-jump-end)
+(add-hook 'ace-jump-mode-end-hook #'ace-mc/maybe-jump-end)
 
-(defun ajmc/reset ()
+(defun ace-mc/reset ()
   "Reset the internal zapping variable flags."
-  (setq ajmc/marking nil))
+  (setq ace-mc/marking nil))
 
-(defun ajmc/keyboard-reset ()
+(defun ace-mc/keyboard-reset ()
   "Reset when `ace-jump-mode' is cancelled.
 Also called when chosen character isn't found while zapping."
   (interactive)
-  (ajmc/reset)
+  (ace-mc/reset)
   (ace-jump-done))
 
 ;;;###autoload
-(defun ajmc/add-multiple-cursors (&optional prefix single-mode)
+(defun ace-mc/add-multiple-cursors (&optional prefix single-mode)
   "Use AceJump to add or remove multiple cursors.
 
-ajmc/add-multiple-cursors will prompt your for locations to
+ace-mc/add-multiple-cursors will prompt your for locations to
 add multiple cursors.  If a cursor already exists at that
 location, it will be removed.  This process continues looping
 until you exit, for example by pressing return or escape. This
@@ -116,21 +116,21 @@ AceJump jumping mode as described in
 or more \\[universal-argument] prefix arguments PREFIX, use the
 corresponding mode from `ace-jump-mode-submode-list'.  For
 example, by default
-   \\[ajmc/add-multiple-cursors] ==> ace-jump-word-mode
-   \\[universal-argument] \\[ajmc/add-multiple-cursors] ==> ace-jump-char-mode
-   \\[universal-argument] \\[universal-argument] \\[ajmc/add-multiple-cursors] ==> ace-jump-line-mode
+   \\[ace-mc/add-multiple-cursors] ==> ace-jump-word-mode
+   \\[universal-argument] \\[ace-mc/add-multiple-cursors] ==> ace-jump-char-mode
+   \\[universal-argument] \\[universal-argument] \\[ace-mc/add-multiple-cursors] ==> ace-jump-line-mode
 
 When the region is active, prompt for AceJump matches based on matching strings."
   (interactive "pi")
   (let* ((index (/ (logb prefix) 2))
 	(submode-list-length (length ace-jump-mode-submode-list)))
-    (setq ajmc/loop-marking (not single-mode))
+    (setq ace-mc/loop-marking (not single-mode))
     (if (< index 0)
         (setq index 0))
     (if (>= index submode-list-length)
         (setq index submode-list-length))
-    (setq ajmc/ace-mode-function (if (use-region-p)
-				     'ajmc/regexp-mode
+    (setq ace-mc/ace-mode-function (if (use-region-p)
+				     'ace-mc/regexp-mode
 				   (nth index ace-jump-mode-submode-list)))
     ;; Sometimes we want to go to different characters. Gets reset with movement.
     ;; TODO: Fix coding convention violation. Accessing a private function. :/
@@ -141,42 +141,42 @@ When the region is active, prompt for AceJump matches based on matching strings.
 	    (exchange-point-and-mark)
 	    (mc/execute-command-for-all-fake-cursors 'exchange-point-and-mark))
 	    (deactivate-mark)
-	  (ajmc/add-char (buffer-substring-no-properties (mark) (point))))
-      (ajmc/add-char (unless (eq ajmc/ace-mode-function 'ace-jump-line-mode)
+	  (ace-mc/add-char (buffer-substring-no-properties (mark) (point))))
+      (ace-mc/add-char (unless (eq ace-mc/ace-mode-function 'ace-jump-line-mode)
 		       (read-char "Query Char:"))))))
 
 ;;;###autoload
-(defun ajmc/add-single-cursor (&optional prefix)
-    "This is a wrapper for `ajmc/add-multiple-cursors', only adding a single cursor.
+(defun ace-mc/add-single-cursor (&optional prefix)
+    "This is a wrapper for `ace-mc/add-multiple-cursors', only adding a single cursor.
 
-PREFIX is passed to `ajmc/add-multiple-cursors', see the documentation there."
+PREFIX is passed to `ace-mc/add-multiple-cursors', see the documentation there."
     (interactive "p")
-    (ajmc/add-multiple-cursors prefix t))
+    (ace-mc/add-multiple-cursors prefix t))
 
-(defun ajmc/regexp-mode (regex)
+(defun ace-mc/regexp-mode (regex)
   "Ace Jump Multiple Cursor with a REGEX."
   (ace-jump-do (regexp-quote regex)))
 
-(defun ajmc/add-char (query-char)
+(defun ace-mc/add-char (query-char)
   "Call `ace-jump-char-mode' with a character QUERY-CHAR and add a cursor at the point."
   (let ((ace-jump-mode-scope 'window))
-    (setq ajmc/marking t
-	  ajmc/query-char query-char)
+    (setq ace-mc/marking t
+	  ace-mc/query-char query-char)
     (if query-char
-	(funcall ajmc/ace-mode-function query-char)
-      (funcall ajmc/ace-mode-function))
+	(funcall ace-mc/ace-mode-function query-char)
+      (funcall ace-mc/ace-mode-function))
     (when overriding-local-map
-      (define-key overriding-local-map [t] 'ajmc/keyboard-reset))))
+      (define-key overriding-local-map [t] 'ace-mc/keyboard-reset))))
 
 ;; Prevent keyboard-reset from being added to mc-list
 ;; mc/cmds-to-run-once
 ;; Use ace-jump-do when the region is active.
 
 (mapc (lambda (el) (add-to-list 'mc/cmds-to-run-once el))
-      '(ajmc/add-char
-	ajmc/keyboard-reset
-	ajmc/add-multiple-cursors
-	ajmc/add-single-cursor
+      '(ace-mc/add-char
+	ace-mc/keyboard-reset
+	ace-mc/add-multiple-cursors
+	ace-mc/add-single-cursor
 	ace-jump-move))
 
 (provide 'ace-mc)
