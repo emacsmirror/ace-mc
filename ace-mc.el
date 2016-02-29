88d5b583b517cda9f16bd6abd0e2ad1850ede76e
Josh Moller-Mara <j.mollermara@nyu.edu>
2016-02-29 01:16:09 -0500

    Update commentary

--- ace-mc.el
+++ ace-mc.el
@@ -23,15 +23,20 @@
 
 ;;; Commentary:
 
-;; ace-mc.el is a package that allows you to add a Multiple Cursors
-;; mode cursor using ace-jump.
+;; ace-mc.el is a package that allows you to quickly add and remove
+;; multiple-cursors mode cursors using ace-jump.
 
-;; This package adds the command `ace-mc/add-multiple-cursors', which
-;; acts like `ace-jump-mode'. It will continue to keep prompting for
-;; places to add cursors until you hit Enter.
-;;
-;; You can use the command `ace-mc/add-single-cursor' for a non-looping
-;; version of `ace-mc/add-multiple-cursors'.
+;; This package adds two commands: `ace-mc/add-multiple-cursors' and
+;; `ace-mc/add-single-cursor'.  Both commands act like
+;; `ace-jump-mode', accepting simliar prefix arguments. However,
+;; instead of jumping to the location, as with ace-jump, the command
+;; will add a new multiple cursor mark. If one is already there, it
+;; will be removed. This makes it easy to remove cursors you've added
+;; accidentally.
+
+;; `ace-mc/add-multiple-cursors' will continue to keep prompting for
+;; places to add or remove cursors until you hit Enter. The command
+;; `ace-mc/add-single-cursor' is a non-looping version.
 
 ;; If you have ace-jump bound on C-0, for example, I recommend the
 ;; following key bindings:
